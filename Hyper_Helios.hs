c0bd83e4fe148a2998a64bbc8e68ae9bfc586dff
Polarit <jarmol@users.noreply.github.com>
2024-09-07 11:02:56 +0300

    Add files via upload
    code improved

--- Hyper_Helios.hs
+++ Hyper_Helios.hs
@@ -1,11 +1,13 @@
-module Main 
+module Main
+
 where
-    
+
 import Data.Time.Clock.POSIX (posixSecondsToUTCTime, utcTimeToPOSIXSeconds)
-import Data.Time.Format (formatTime, defaultTimeLocale)
-import Data.Time ( toGregorian, UTCTime(utctDay), getCurrentTime )
-import Numeric ( showFFloat, showEFloat )
-import SolarCurrent (julianCentury
+import Data.Time.Format (formatTime )
+import Data.Time ( toGregorian, UTCTime(utctDay), getCurrentTime, utcToLocalTimeOfDay )
+import Numeric ( showFFloat )
+import SolarCurrent
+    (julianCentury
     , sunDeclin
     , solarNoonLST
     , sunriseLST
@@ -13,59 +15,64 @@ import SolarCurrent (julianCentury
     , showtime
     , solarZenithAngle
     , atmosRefract
-    , solAzimuth, hourAngle
+    , solAzimuth, hourAngle, sunlightDuration
+    , nonIntRem
+    , showLocalTime
  )
 
-getSeconds :: Integral b => UTCTime -> b
-getSeconds ct =
-    let posixSeconds = utcTimeToPOSIXSeconds ct
-    in  round posixSeconds
 
--- General data: time zone, latitude & longitude
-timeZone :: Double
-timeZone = 2
-
+-- General data:  latitude & longitude
 latitude :: Double
 latitude = 65.85
 
 longitude :: Double
 longitude = 24.18
 
+
+timeZone = 2.0
+
 main :: IO ()
 main = do
     currentTime <- getCurrentTime
     let (year, month, day) = toGregorian . utctDay $ currentTime
-        ps = round $ utcTimeToPOSIXSeconds currentTime
-        hours = read $ show $ mod (div ps 3600) 24
-        minutes = read $ show $ mod (div ps 60) 60
-        seconds = read $ show $ mod ps 60
+        ps = utcTimeToPOSIXSeconds currentTime
+        hours = nonIntRem (ps / 3600) 24
+        minutes = nonIntRem (ps / 60) 60
+        seconds =  nonIntRem ps 60
+        lochour = hours +  timeZone
+        posMinutes = 60 * lochour
+        decMinutes = 0.001 * fromInteger (round $ 1000 * posMinutes + 5)
         jC = julianCentury currentTime
         sunDecl = sunDeclin jC
-        solNoon = solarNoonLST jC 2.0 longitude
         sunRise = sunriseLST jC 2.0 latitude longitude
+        solNoon = solarNoonLST jC 2.0 longitude
         sunSet = sunsetLST jC 2.0 latitude longitude
-        lochour = hours + timeZone
-        hrAngle = hourAngle posMinutes 2.0  jC longitude
-        posMinutes =  (60*lochour + minutes) + seconds/60.0
-        solZen = solarZenithAngle posMinutes 2.0 jC latitude longitude
+        sunLD = sunlightDuration jC latitude
+        hrAngle = hourAngle decMinutes 2.0  jC longitude
+        solZen = solarZenithAngle decMinutes 2.0 jC latitude longitude
         solElevat = 90.0 - solZen
-        refract = atmosRefract posMinutes timeZone jC latitude longitude
+        refract = atmosRefract decMinutes 2.0 jC latitude longitude
         solAz = solAzimuth hrAngle latitude solZen sunDecl
         (blue, green, red, yellow, white, black, bluebg, whitebg,blackbg) = ("\ESC[94m","\ESC[92m","\ESC[91m","\ESC[93m","\ESC[97m","\ESC[30m","\ESC[44m","\ESC[107m","\ESC[40m" )
     putStrLn (white ++ bluebg)
     putStrLn "  OBSERVATION LOCATION  "
-    putStrLn ("  Latitude: " ++ show latitude ++ " . Longitude: " ++ show longitude ++ " Timezone: " ++ show timeZone)
-    putStr "  Date and Current Time "
-    putStrLn $ formatTime defaultTimeLocale "%y-%m-%d %H:%M:%S" currentTime
-    putStrLn $ "  Local time  " ++ showtime posMinutes ++ " UTC + " ++ show timeZone ++ " h"
+    putStrLn ("  Latitude: " ++ show latitude
+     ++ "  Longitude: " ++ show longitude
+      ++ " Timezone: " ++ show (truncate timeZone))
+    putStrLn $ "  Date and Current Time " ++ show currentTime
+    putStrLn $ "  Local time  " ++ showLocalTime lochour minutes seconds
+       ++ " ( UTC + " ++ show (truncate timeZone) ++ " h )"
     putStrLn (yellow ++ blackbg ++  "\n  CURRENT SOLAR POSITION")
     putStrLn $ "  Sun Declination "       ++ showFFloat (Just 4) sunDecl "°"
     putStrLn $ "  Sunrise    " ++ showtime sunRise
     putStrLn $ "  Solar Noon " ++ showtime solNoon
     putStrLn $ "  Sunset     " ++ showtime sunSet
+    putStrLn $ "  Sunlight duration " ++ showFFloat (Just 3) (sunLD / 60) " hour"
+    putStrLn $ "  Solar Zenith angle " ++ showFFloat (Just 4) solZen  "°"
     putStrLn $ "  Solar elevation angle " ++ showFFloat (Just 4) solElevat  "°"
     putStrLn $ "  Atmospheric refraction " ++ showFFloat (Just 4) refract  "°"
-    putStrLn $ "  Refraction corrected elevation " ++ showFFloat (Just 4) (solElevat + refract) "°" 
-    putStrLn $ "  Solar Azimuth angle " ++ showFFloat (Just 3) solAz  "°" 
-    putStrLn (black ++ whitebg ++ ".")
+    putStrLn $ "  Refraction corrected elevation " ++ showFFloat (Just 4) (solElevat + refract) "°"
+    putStrLn $ "  Solar Azimuth angle " ++ showFFloat (Just 3) solAz  "°"
+    putStrLn (green ++ ".")
+
     -- About colours https://i.sstatic.net/9UVnC.png
