6d2e8f07061706afe56aaadda5862a1c0a65a01c
Polarit <jarmol@users.noreply.github.com>
2024-10-22 14:58:43 +0300

    print multiple lines with a single putStrLn

--- leapyear.hs
+++ leapyear.hs
@@ -1,5 +1,12 @@
 module Leapyear (main)
 where
+{-|
+Every year that is exactly divisible by four is a leap year, except for years
+that are exactly divisible by 100, but these centurial years are leap years if
+they are exactly divisible by 400. For example, the years 1700, 1800, and 1900
+are not leap years, but the years 1600 and 2000 are.
+See https://en.wikipedia.org/wiki/Leap_year#Gregorian_calendar
+-}
 
 leapyear :: Integral a => a -> String
 leapyear year
@@ -12,14 +19,12 @@ makeRow :: Int -> String
 makeRow year =
    "The year " ++ show year ++ leapyear year
 
+ylist :: [Int]
 ylist = [1900,2000,2012] ++ [2022..2026]
 
-
-rlist :: [String]
-rlist  = map makeRow ylist 
-slist :: [[Char]]
-slist = map (++ "\n") rlist
+tlist :: String
+tlist = unlines $ map makeRow ylist
 
 main :: IO ()
 main =
-   putStrLn $ concat slist
+   putStrLn tlist
