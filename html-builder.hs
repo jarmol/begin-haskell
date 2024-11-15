547f0c8d96ddbec34ae12f15c2eccce077248218
Polarit <jarmol@users.noreply.github.com>
2024-11-15 12:29:37 +0200

    added source link

--- html-builder.hs
+++ html-builder.hs
@@ -43,6 +43,7 @@ main = do
       <> h2_"Here you see, how to make html with Haskell."
       <> p_ (a_ "https://learn-haskell.blog/03-html/01-html_content.html" "Learn Haskell by building a blog generator")
       <> p_ (a_ "https://suncalc.lammi.cc/" "Homepage")
+      <> p_ (a_ "https://github.com/jarmol/begin-haskell/blob/main/html-builder.hs" "Html-builder Haskell-source")
       <> p_ "Made with GHCi 9.4.8"
      ))
 
